3da00e75e252bdbed2ac73025b4a57106033a5b3
Zohirul <zhremon@outlook.com>
2026-05-07 00:21:03 +0600

    snake_case rust follows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
 }
+
+//snake_case: hello_world - rust follows this convention for function and variable names
+//camelCase: helloWorld Java follows this convention for function and variable names
+//PascalCase: HelloWorld - Rust follows this convention for struct and enum names, as well as for module names
+//SCREAMING_SNAKE_CASE: HELLO_WORLD - Rust follows this convention for constant names
