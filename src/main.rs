d95c14d0b3946d4264d4c431beb126e94a9b3a6a
Zohirul <zhremon@outlook.com>
2026-05-08 00:26:44 +0600

    Hands on Data types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-fn main() {
-    println!("Hello, world!");
-}
+// fn main() {
+//     println!("Hello, world!");
+// }
 
 //snake_case: hello_world - rust follows this convention for function and variable names
 //camelCase: helloWorld Java follows this convention for function and variable names
 //PascalCase: HelloWorld - Rust follows this convention for struct and enum names, as well as for module names
 //SCREAMING_SNAKE_CASE: HELLO_WORLD - Rust follows this convention for constant names
+
+// Data types
+fn main() {
+    let num: u8 = 7;
+    println!("The number is: {}", num);
+}
